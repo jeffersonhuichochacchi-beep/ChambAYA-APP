f641e4ef35252835abcbf9b8b5d85565726dd1b1
sukeresmith-9 <sukeresmith.9@gmail.com>
2026-09-02 17:18:30 -0500

    actualizacion de manejo de barras del sistema

--- app/src/main/java/com/example/chambaya/MainActivity.kt
+++ app/src/main/java/com/example/chambaya/MainActivity.kt
@@ -43,7 +43,15 @@ class MainActivity : AppCompatActivity() {
 
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            // Only pad top, left, right. Bottom remains 0 to allow edge-to-edge drawing over nav bar
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.bottomNavigation) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            // Push bottom nav items up while keeping the background stretched to the bottom edge
+            v.setPadding(v.paddingLeft, v.paddingTop, v.paddingRight, systemBars.bottom)
             insets
         }
 
